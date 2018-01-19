494f409e2bb7276925d4b95158c0e2ed048289f6
unknown <chowcy@umich.edu>
2018-01-19 00:05:55 -0500

    switched to v2 project page, add header shadow, color changes

---
+++ projects-v1.php
@@ -0,0 +1,178 @@
+<html lang="en">
+<?php $title='Projects'; include("head.php");?>
+<?php $projects = true; include("header.php");?>
+	<main id="main">
+		<div class="container">
+
+			<div class="row">
+
+				<div class="col-xs-12 col-sm-6 col-lg-4">
+					<section class="project">
+						<h3>Free Your Stuff</h3>
+						<h4>Interaction Design. Prototyping.</h4>
+						<div class="overlay-container">
+							<img src="img/freeyourstufflogo.jpg" alt="">
+							<a href="freeyourstuff.php" hreflang="en" target="_blank">
+								<div class="overlay">
+									<p>mobile app design to share community resources.</p>
+								</div>
+							</a>
+						</div>
+					</section>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-lg-4">
+					<section class="project">
+						<h3>Harman Consulting</h3>
+						<h4>UX Research. Needs Assessment.</h4>
+						<div class="overlay-container">
+							<img src="img/harman.jpg" alt="">
+							<a href="harman.php" hreflang="en" target="_blank">
+								<div class="overlay">
+									<p>usability research on an in-vehicle infotainment system.</p>
+								</div>
+							</a>
+						</div>
+					</section>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-lg-4">
+					<section class="project">
+						<h3>SmartVision</h3>
+						<h4>UX Design. Prototyping. App Dev.</h4>
+						<div class="overlay-container">
+							<img src="img/smartvision.jpg" alt="">
+							<a href="smartvision.php" hreflang="en" target="_blank">
+								<div class="overlay">
+									<p>mobile app design/dev for low-vision people.</p>
+								</div>
+							</a>
+						</div>
+					</section>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-lg-4">
+					<section class="project">
+						<h3>Mobile Site Optimization</h3>
+						<h4>Responsive Web Design. CSS. </h4>
+						<div class="overlay-container">
+							<img src="img/newberry.jpg" alt="">
+							<a href="https://seelio.com/w/2wdn/the-newberry-library-development-office" hreflang="en" target="_blank">
+								<div class="overlay">
+									<p>responsive web redesign for Chicago's Newberry Library.</p>
+								</div>
+							</a>
+						</div>
+					</section>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-lg-4">
+					<section class="project">
+						<h3>Graphic Design</h3>
+						<h4>Graphic Design. Typography.</h4>
+
+						<div class="overlay-container">
+							<img src="img/floral.jpg" alt="">
+							<a href="graphicdesign.php" hreflang="en" target="_blank">
+								<div class="overlay">
+									<p>Various graphic design samples. </p>
+								</div>
+							</a>
+						</div>
+					</section>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-lg-4">
+					<section class="project">
+						<h3>Music Recommender</h3>
+						<h4>Python, APIs.</h4>
+						<div class="overlay-container">
+							<img src="img/music.jpg" alt="">
+							<a href="https://github.com/chowcy/MusicRecommender" hreflang="en" target="_blank">
+								<div class="overlay">
+									<p>recommends music using the Facebook and Spotify APIs. Click for code.</p>
+								</div>
+							</a>
+						</div>
+					</section>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-lg-4">
+					<section class="project">
+						<h3>openAIR Web Challenge</h3>
+						<h4>Web UX Design, HTML/CSS, PHP.</h4>
+						<div class="overlay-container">
+							<img src="img/openair.png" alt="">
+							<div class="overlay">
+								<p>accessibility redesign of SummitIDG's website.</p>
+							</div>
+						</div>
+					</section>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-lg-4">
+					<section class="project">
+						<h3>Ann Arbor Data Dive</h3>
+						<h4>Python, Data Retrieval.</h4>
+						<div class="overlay-container">
+							<img src="img/datadive.jpg" alt="">
+							<a href="https://github.com/chowcy/GLOSWaterTempDataBuoy" hreflang="en" target="_blank">
+								<div class="overlay">
+									<p>gets water temperature data for Great Lakes buoys. Click for code.</p>
+								</div>
+							</a>
+						</div>
+					</section>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-lg-4">
+					<section class="project">
+						<h3>Conway's Game of Life</h3>
+						<h4>HTML/CSS, JavaScript, Graphics.</h4>
+						<div class="overlay-container">
+							<img src="img/gol.jpg" alt="">
+							<div class="overlay">
+								<p>custom, colored Game of Life simulation. In collaboration with Dhananjaya Premawardena, Sam Dunham.</p>
+							</div>
+						</div>
+					</section>
+				</div>
+<!--
+				<div class="col-xs-12 col-sm-6 col-lg-4">
+					<section class="project">
+						<h3>RGB Consulting</h3>
+						<h4>User Research, Contextual Inquiry.</h4>
+						<div class="overlay-container">
+							<img src="img/rgb.jpg" alt="">
+							<div class="overlay">
+								<p>consulting for a non-profit after conducting contextual research. With Janet Li, Tianyi Liu, Paul Suvansupa.</p>
+							</div>
+						</div>
+					</section>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-lg-4">
+					<section class="project" onclick="displayContent(this);" onmouseover="hover(this);" onmouseout="normal(this);" onkeypress="displayContent(this);" tabindex="0" onfocus="hover(this);" onblur="normal(this);">
+						<h3>2016 InfoGamers Design Jam</h3>
+						<h4>Design</h4>
+						<div class="img-wrapper"><img src="img/infogamers.jpg" alt=""></div>
+						<p>a free-to-play design of Just Dance. First/Second Place. In collaboration with Wei Cai, Ruta Gokhale, Aakanksha Parameshwar, Olivia Patercsak.</p>
+					</section>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-lg-4">
+					<section class="project" onclick="displayContent(this); "onmouseover="hover(this);" onmouseout="normal(this);" onkeypress="displayContent(this);" tabindex="0" onfocus="hover(this);"  onblur="normal(this);">
+						<h3>UMSI Orientation: Flint Water Crisis</h3>
+						<h4>Design</h4>
+						<div class="img-wrapper"><img src="img/orientation.jpg" alt=""></div>
+						<p>a campaign design to sustain donations. In collaboration with Amy Chen, Gary Cheung, Jasmine Duan, Lauren Sheridan.</p>
+					</section>
+				</div>
+-->
+
+			</div>
+		</div>
+	</main>
+	<?php include("footer.php");?>
+</body>
+</html>
