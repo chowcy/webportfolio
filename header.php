f8b11f0638466f89fb5b929b9323d6e83e4b0b43
unknown <chowcy@umich.edu>
2018-01-29 11:39:54 -0500

    reduce fontsize to resolve nav-right wrap bug on mobile

--- header.php
+++ header.php
@@ -3,9 +3,9 @@
 			<nav>
 				<a href="index.php"><h1 class="navbar-header"><span id="name">CATHY Y CHOW</span>   <span id="name-subtitle">UX Designer and Developer</span></h1></a>
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="index.php" hreflang="en" class=<?php if ($about) { echo "active";}?>><h2>About</h2></a></li>
-					<li><a href="projects.php" hreflang="en" class=<?php if ($projects) { echo "active";}?>><h2>Projects</h2></a></li>
-					<li><a href="resume.php" hreflang="en" class=<?php if ($resume) { echo "active";}?>><h2>Resume</h2></a></li>
+					<li><a href="index.php" hreflang="en" class=<?php if ($about) { echo "active";} else { echo '""';}?>><h2>About</h2></a></li>
+					<li><a href="projects.php" hreflang="en" class=<?php if ($projects) { echo "active";} else { echo '""';}?>><h2>Projects</h2></a></li>
+					<li><a href="resume.php" hreflang="en" class=<?php if ($resume) { echo "active";} else { echo '""';}?>><h2>Resume</h2></a></li>
 				</ul>
 			</nav>
 		</header>
