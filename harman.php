c388eb1f248138a5dfc9138de9658952833a23ae
unknown <chowcy@umich.edu>
2018-03-13 21:16:15 -0400

    add context/overview to harman project

--- harman.php
+++ harman.php
@@ -3,60 +3,106 @@
 	<?php $title='Harman Consulting'; include("head.php");?>
 	<?php $projects = true; include("header.php");?>
 	<main id="main">
-		<section class="portfolio-section">
-			<h3>Harman Consulting</h3>
-			<h4>UX Researcher. Consultant.</h4>
-			<p>user research project for an in-vehicle infotainment system made by Harman International.</p>
+		<section class="portfolio-section portfolio-overview">
+			<div class="centerize">
+				<div class="title-section">
+					<h3>Harman Consulting</h3>
+					<h4>UX Research. Consulting.</h4>
+				</div>
+				<div class="row context">
+					<div class="col-xs-12 col-md-12 col-lg-6">
+						<h4>Overview</h4>
+						<p>As consultants for Harman International Industries, our team conducted usability research on one of their in-vehicle infortainment systems. Our goals were to 1) understand the user population - those who drive or ride in cars - and how they connect to their cars and infotainment systems, as well as to 2) evaluate the usability of the system. This project is under a non-disclosure agreement, so certain product details and research findings have been partially omitted.</p>
+					</div>
+					<div class="col-xs-12 col-md-12 col-lg-6">
+						<a href="img/tile-harman.jpg" data-lightbox="harman" data-title="Graphic for Harman Connected Cars">
+							<img src="img/tile-harman.jpg" width='100%' alt="">
+						</a>
+					</div>
+				</div>
+				<div class="row context">
+					<div class="col-xs-12 col-md-6 col-lg-4">
+						<h4>Role</h4>
+						<p>UX Researcher</p>
+						<h4>Time Frame</h4>
+						<p>Jan 2017 - Apr 2017</p>
+					</div>
+					<div class="col-xs-12 col-md-6 col-lg-4">
+						<h4>Context</h4>
+						<p>Applied Team Project</p>
+						<h4>Methods</h4>
+						<p>Interviews, Surveys, Personas, Heuristic Evaluations, Usability Testing</p>
+					</div>
+					<div class="col-xs-12 col-md-6 col-lg-4">
+						<h4>Skills</h4>
+						<p>Interviewing, analyzing data, developing protocols, piloting surveys, writing reports, public speaking and presentations</p>
+					</div>
+				</div>
+			</div>
 		</section>
 
 		<section class="portfolio-section">
-			<h3>Competitive Analysis</h3>
-			<img src="http://media.sproutsocial.com/uploads/2017/03/Social-Media-Competitive-Analysis.png" alt="">
-			<aside>Image from sproutsocial</aside>
-			<p>In order to better understand the features of current infotainment systems, my team conducted a competitive analysis. We compared Harman's system with similar products by Panasonic and Bosch, as well as indirect competitors such as Android Auto, Apple CarPlay, Bose Automotive, and BeatsAudio. </p>
+			<div class="centerize">
+				<h3>Competitive Analysis</h3>
+				<img src="http://media.sproutsocial.com/uploads/2017/03/Social-Media-Competitive-Analysis.png" alt="">
+				<aside>Image from sproutsocial</aside>
+				<p>In order to better understand the features of current infotainment systems, my team conducted a competitive analysis. We compared Harman's system with similar products by Panasonic and Bosch, as well as indirect competitors such as Android Auto, Apple CarPlay, Bose Automotive, and BeatsAudio. </p>
+			</div>
 		</section>
 
 		<section class="portfolio-section">
-			<h3>Interviews</h3>
-			<a href="img/affinitywall622.jpg" data-lightbox="harman" data-title="Snippet of our affinity wall">
-				<img src="img/affinitywall622.jpg" alt="Part of our Affinity Wall" width="50%">
-			</a>
-			<p>To understand the user population's attitudes, motivations, and behaviors, we conducted four interviews with members of the target user population: drivers and passengers that interact with car infotainment systems. </p>
-			<p>My team held interpretation sessions where we extracted key findings and patterns from our interviews onto sticky notes. We created an affinity wall, putting notes with common themes together so we could see the broader picture and relationships and ultimately generate recommendations.</p>
+			<div class="centerize">
+				<h3>Interviews</h3>
+				<a href="img/affinitywall622.jpg" data-lightbox="harman" data-title="Snippet of our affinity wall">
+					<img src="img/affinitywall622.jpg" alt="Part of our Affinity Wall" width="50%">
+				</a>
+				<p>To understand the user population's attitudes, motivations, and behaviors, we conducted four interviews with members of the target user population: drivers and passengers that interact with car infotainment systems. </p>
+				<p>My team held interpretation sessions where we extracted key findings and patterns from our interviews onto sticky notes. We created an affinity wall, putting notes with common themes together so we could see the broader picture and relationships and ultimately generate recommendations.</p>
+			</div>
 		</section>
 
 		<section class="portfolio-section">
-			<h3>Surveys</h3>
-			<img src="img/surveyboard622.jpg" alt="" width="50%">
-			<p>With the key findings from our interviews in mind, we designed survey questions to assess how prevalent driver behaviors and attitudes were and to learn about the user demographics. From our 154 survey responses, we learned of four key findings related to preferred infotainment interaction interfaces, most commonly used infotainment features, and attitudes about phone-car connections.</p>
+			<div class="centerize">
+				<h3>Surveys</h3>
+				<img src="img/surveyboard622.jpg" alt="" width="50%">
+				<p>With the key findings from our interviews in mind, we designed survey questions to assess how prevalent driver behaviors and attitudes were and to learn about the user demographics. From our 154 survey responses, we learned of four key findings related to preferred infotainment interaction interfaces, most commonly used infotainment features, and attitudes about phone-car connections.</p>
+			</div>
 		</section>
 
 		<section class="portfolio-section">
-			<h3>Personas and Scenarios</h3>
-			<a href="img/personas622.jpg" data-lightbox="harman" data-title="Images of our personas">
-				<img src="img/personas622.jpg" alt="Images of our Personas" width="50%">
-			</a>
-			<aside>Graphics by Raden</aside>
-			<p>Our surveys and interviews helped us understand the user population and the contexts in which users interact with infotainment systems. We summarized this information into personas and scenarios, which are design artifacts we constructed to guide our thinking during the evaluation process. Next, we turned to heuristic evaluations and usability testing to assess the usability of Harman's product. </p>
+			<div class="centerize">
+				<h3>Personas and Scenarios</h3>
+				<a href="img/personas622.jpg" data-lightbox="harman" data-title="Images of our personas">
+					<img src="img/personas622.jpg" alt="Images of our Personas" width="50%">
+				</a>
+				<aside>Graphics by Raden</aside>
+				<p>Our surveys and interviews helped us understand the user population and the contexts in which users interact with infotainment systems. We summarized this information into personas and scenarios, which are design artifacts we constructed to guide our thinking during the evaluation process. Next, we turned to heuristic evaluations and usability testing to assess the usability of Harman's product. </p>
+			</div>
 
 		</section>
 		<section class="portfolio-section">
-			<h3>Heuristic Evaluation</h3>
-			<img src="https://public-media.interaction-design.org/images/uploads/bc407d74dd91972a7370c0e22cfacc36.jpg" alt="" width="50%">
-			<aside>Image from the Interaction Design Foundation</aside>
-			<p>Using Nielsen's (1994) ten Heuristics, each member of my team evaluated Harman's system and rated the level of severity of the usability issue. We then aggregated our findings and prioritized them before describing them in our report.</p>
+			<div class="centerize">
+				<h3>Heuristic Evaluation</h3>
+				<img src="https://public-media.interaction-design.org/images/uploads/bc407d74dd91972a7370c0e22cfacc36.jpg" alt="" width="50%">
+				<aside>Image from the Interaction Design Foundation</aside>
+				<p>Using Nielsen's (1994) ten Heuristics, each member of my team evaluated Harman's system and rated the level of severity of the usability issue. We then aggregated our findings and prioritized them before describing them in our report.</p>
+			</div>
 		</section>
 
 		<section class="portfolio-section">
-			<h3>Usability Testing</h3>
-			<img src="img/usabilitytest622.jpg" alt="usability test methods" width="35%">
-			<p>The last research method we conducted was usability testing with five users. We created five tasks for users to complete using Harman's system, and we asked the participants to think aloud about their thought process and expectations while they did it. We observed and recorded comments, errors, and notable incidents. With our notes and responses from questionnaires we gave them, we concluded four key findings related to the phone-car connection process, home screen interface, and radio interface.</p>
+			<div class="centerize">
+				<h3>Usability Testing</h3>
+				<img src="img/usabilitytest622.jpg" alt="usability test methods" width="35%">
+				<p>The last research method we conducted was usability testing with five users. We created five tasks for users to complete using Harman's system, and we asked the participants to think aloud about their thought process and expectations while they did it. We observed and recorded comments, errors, and notable incidents. With our notes and responses from questionnaires we gave them, we concluded four key findings related to the phone-car connection process, home screen interface, and radio interface.</p>
+			</div>
 		</section>
 
 		<section class="portfolio-section">
-			<h3>The Team</h3>
-			<img src="img/team622.jpg" alt="team" width="50%">
-			<p>Special thanks to my wonderful teammates <a href="http://kaiwensun.me" hreflang="en" target="_blank">Kaiwen Sun</a>, <a href="http://radentonev.com" hreflang="en" target="_blank">Raden Tonev</a>, and Seungjoo David Baek for their contributions.</p>
+			<div class="centerize">
+				<h3>The Team</h3>
+				<img src="img/team622.jpg" alt="team" width="50%">
+				<p>Special thanks to my wonderful teammates <a href="http://kaiwensun.me" hreflang="en" target="_blank">Kaiwen Sun</a>, <a href="http://radentonev.com" hreflang="en" target="_blank">Raden Tonev</a>, and Seungjoo David Baek for their contributions.</p>
+			</div>
 		</section>
 	</main>
 	<?php include("footer.php");?>
